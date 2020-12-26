df04de2483ea412fb02875e15ca7ab658147edc8
Farooq <farooqalaulddinuc@gmail.com>
2020-12-26 02:10:11 -0800

    fix

--- api/init.php
+++ api/init.php
@@ -11,7 +11,7 @@ session_start();
 define('DS', DIRECTORY_SEPARATOR);
 define('DIR', dirname(__DIR__));
 define('THREE_MONTHS_COOKIE_EXPIRY', 7776000); // 90 days
-define('JWTsecret__key', 'crfujxn2r5u8x/A?DG-KaPdSgVkYp3s6v9y$B&E)H@MbQeThWmZq4t7w!z%C*F-JaNdRfUjXn2r5u8x/A?D(G+KbPeShVkYp3s6v9y$B&E)H@McQQeThWmZqfTjWnZq4t7w!z%C*F-JaNdRgUkXp2s5u8x/A?D(G+KbPeShVmYq3t6w9z$B&E)H@McQfTjWnZr4u7x!A%D*F-JaNdRgUkXp2s5v8y/B?E(H+KbPeShVmYq3t6w9z$C&F)J@NcQfTjWnZr4u');
+define('JWTsecret__key', '`ADD YOUR KEY HERE`');
 
 require "..".DS."..".DS."..".DS."Struct".DS."_init.php";
 require DIR.DS.'vendor'.DS.'autoload.php';
